17ce2cd92a83c778f3f45cedb532b6cb52e97e45
dama-nation <adamachide@gmail.com>
2026-07-21 09:08:40 +0100

    Auth Controllers Added

--- backend/main.js
+++ backend/main.js
@@ -2,11 +2,15 @@ import express from "express"
 import authRoutes from "./routes/auth.js"
 import dotenv from 'dotenv'
 import connectMongoDB from './db/connect.js'
+import cookieparser from 'cookie-parser'
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.json()) // Middleware to parse req.body
+app.use(express.urlencoded({extended: true})) //parse form data(urlencoded)
+app.use(cookieparser())
 
 app.use('/api/auth', authRoutes);
 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { signup, login, logout } from '../controllers/authController.js';
+import { protectRoute } from '../middleware/protectRoute.js';
+import { signup, login, logout, getMe } from '../controllers/authController.js';
 
 const router = express.Router();
 
+router.get('/me', protectRoute, getMe)
 router.post('/signup', signup)
 router.post('/login', login)
 router.post('/logout', logout)
