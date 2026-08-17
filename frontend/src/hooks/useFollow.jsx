92b95f95cf7f754069bd25a1d29298453f758971
dama-nation <adamachide@gmail.com>
2026-08-17 09:52:28 +0100

    User Following Added

---
+++ frontend/src/hooks/useFollow.jsx
@@ -0,0 +1,32 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
+
+const useFollow = () => {
+    const queryClient = useQueryClient()
+    const { mutate: follow, isPending } = useMutation({
+        mutationFn: async (userId) => {
+          try{
+            const res = await fetch(`/api/users/follow/${userId}`,{
+              method: 'POST'
+            })
+            const data = res.json();
+            if(!res.ok) throw new Error(data.message || 'Something Went Wrong')
+            return data;
+          }catch(error){
+            throw new Error(error.message)
+          }
+        },
+        onSuccess: () => {
+          Promise.all([
+            queryClient.invalidateQueries({queryKey: ['suggestedUsers']}),
+            queryClient.invalidateQueries({queryKey: ['authUser']})
+          ])
+        },
+        onError: () => {
+          toast.error('Something went wrong')
+        }
+    })
+  return { follow, isPending }
+}
+
+export default useFollow
